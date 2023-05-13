8c8666e0b388d8eb13f843f4f9e7196495ca7a3c
kardios <132521921+kardios@users.noreply.github.com>
2023-05-13 13:08:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def chatgpt_conversation(conversation_log):
     return conversation_log
 
 st.write("**Readhacker** version 0.01"
-st.write("*by citizen developer Loy Hui Chien*")
+st.write("by citizen developer Loy Hui Chien")
 input_text = st.text_area('I am your helpful GPT4-powered reading assistant. Enter the text that you want me to analyse for you...', '')
 conversations = []
 
