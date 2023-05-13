c328de20f8d0933896a61df5dc61addf3a1b197b
kardios <132521921+kardios@users.noreply.github.com>
2023-05-13 23:00:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ import streamlit as st
 API_KEY = os.environ["OPENAI_KEY"]
 openai.api_key = API_KEY
 model_id = 'gpt-4'
+temperature = 0.3
 
 def chatgpt_conversation(conversation_log):
     response = openai.ChatCompletion.create(
-        model=model_id,
-        temperature=0,
-        messages=conversation_log
+        model = model_id,
+        temperature = temperature,
+        messages = conversation_log
     )
     conversation_log.append({
         'role': response.choices[0].message.role, 
@@ -20,9 +21,8 @@ def chatgpt_conversation(conversation_log):
     })
     return conversation_log
 
-st.write("**Readhacker** version 0.01")
-st.write("*by citizen developer Loy Hui Chien*")
-input_text = st.text_area('I am your helpful GPT4-powered reading assistant. Enter the text that you want me to analyse for you...', '')
+st.write("**Readhacker** version 0.1 by Sherwood Analytica")
+input_text = st.text_area('I am your helpful GPT4-powered reading assistant. Enter the text that you want me to analyse for you.', '')
 conversations = []
 
 if st.button('Summarise'):
@@ -40,22 +40,9 @@ if st.button('Summarise'):
 
 if st.button('Key Trends'):
   start = time.time()
-  prompt_text = "Using bullet points, extract key trends and data"
+  prompt_text = "Extract key trends and data"
   prompt = prompt_text + ":\n" + input_text
-  conversations.append({'role': 'system', 'content': 'You are my helpful reading assistant. You will read the text I provide and generate a list of key trends in bullet points.'})
-  conversations.append({'role': 'user', 'content': prompt})
-  conversations = chatgpt_conversation(conversations)
-  output_text = conversations[-1]['content']
-  end = time.time()
-  st.write(output_text)
-  conversations = []
-  st.write("*Time to generate*: " + str(round(end-start,2)) + " seconds")
-
-if st.button('All Events'):
-  start = time.time()
-  prompt_text = "Generate a timeline of events"
-  prompt = prompt_text + ":\n" + input_text
-  conversations.append({'role': 'system', 'content': 'You are my helpful reading assistant. You will read the text I provide and generate a timeline of events.'})
+  conversations.append({'role': 'system', 'content': 'You are my helpful reading assistant. You will read the text I provide and generate a list of key trends and data in bullet points.'})
   conversations.append({'role': 'user', 'content': prompt})
   conversations = chatgpt_conversation(conversations)
   output_text = conversations[-1]['content']
@@ -66,9 +53,9 @@ if st.button('All Events'):
 
 if st.button('Bias Check'):
   start = time.time()
-  prompt_text = "Highlight possible biases"
+  prompt_text = "Highlight any possible biases"
   prompt = prompt_text + ":\n" + input_text
-  conversations.append({'role': 'system', 'content': 'You are my helpful reading assistant. You will read the text I provide. Highlight possible biases.'})
+  conversations.append({'role': 'system', 'content': 'You are my helpful reading assistant. You will read the text I provide and highlight any possible biases.'})
   conversations.append({'role': 'user', 'content': prompt})
   conversations = chatgpt_conversation(conversations)
   output_text = conversations[-1]['content']
@@ -108,6 +95,20 @@ if st.button('Alternative'):
   prompt_text = "Offer perspectives that are missing"
   prompt = prompt_text + ":\n" + input_text
   conversations.append({'role': 'system', 'content': 'You are my helpful reading assistant. You will read the text I provide and offer perspectives that are missing.'})
+  conversations.append({'role': 'user', 'content': prompt})
+  conversations = chatgpt_conversation(conversations)
+  output_text = conversations[-1]['content']
+  end = time.time()
+  st.write(output_text)
+  conversations = []
+  st.write("*Time to generate*: " + str(round(end-start,2)) + " seconds")
+
+prompt_text_input = st.text_input('Customise your prompt', 'Assign a grading between A+ to F, and explain why')
+if st.button('Customise'):
+  start = time.time()
+  prompt_text = prompt_text_input
+  prompt = prompt_text + ":\n" + input_text
+  conversations.append({'role': 'system', 'content': 'You are my helpful reading assistant. You will read the text I provide. ' + prompt_text_input})
   conversations.append({'role': 'user', 'content': prompt})
   conversations = chatgpt_conversation(conversations)
   output_text = conversations[-1]['content']
