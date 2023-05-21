86849970b3723d338e26f225ba3253d22f799281
kardios <132521921+kardios@users.noreply.github.com>
2023-05-21 11:40:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.write("**Readhacker** Beta 0.1 : An AI-Powered Reading Assistant By **Sherwoo
 model_id = st.radio("Compared to GPT-3.5, GPT-4 is smarter and can handle longer inputs, while GPT-3.5 is faster at generating responses. Exceeding the length limit will result in errors. Choose one of the Large Language Models for your AI-powered reading.", ('gpt-4','gpt-3.5-turbo'))
 temperature = 0
 #temperature = st.slider('Increase temperature for more creative output',0.0,1.0,0.1,0.1)
-input_text = st.text_area("I am your AI-powered reading assistant. Enter the text you want me to analyse in the box. Whether it\'s a news article, speech, or commentary, I\'ll do my best to assist you. My average processing time is usually under a minute.","")
+input_text = st.text_area("I am your AI-powered reading assistant. Enter the text you want me to analyse in the box. Whether it\'s a news article, a speech, or a commentary, I\'ll do my best to assist you. My average processing time is usually below one minute.","")
 conversations = []
 
 def chatgpt_conversation(conversation_log):
